8cdeae1fc2387a4c06a140b3e46b575ea81596e8
Techy-Monarch <otuemicheal1000@gmail.com>
2023-11-06 16:10:00 -0800

    init commit

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -21,7 +21,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 
   return (
     <div
-      onClick={() => router.push(`/product/${data.id}`)}
+      onClick={() => router.push(`/product/${data.Id}`)}
       className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm"
     >
       <div className="flex flex-col item-center w-full gap-1">
